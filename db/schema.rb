f5081765775c7e04adc906db18841d1457b45e68
Kasper <kasperbak89@pm.me>
2023-11-02 15:51:58 +0100

    Implement a basic users/posts/comments models and relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_02_141231) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_02_142644) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -34,5 +38,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_02_141231) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
